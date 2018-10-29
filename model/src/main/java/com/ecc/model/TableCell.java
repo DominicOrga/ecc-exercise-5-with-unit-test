30b03a49e568708531b78776ed7855e95e3f6504
Dominic Orga <dominicorga@gmail.com>
2018-10-29 08:13:24 +0800

    Create TableCell POJO

---
+++ model/src/main/java/com/ecc/model/TableCell.java
@@ -0,0 +1,40 @@
+package com.ecc.model;
+
+public class TableCell implements Comparable<TableCell> {
+		private String leftCell;
+		private String rightCell;
+
+		public TableCell(String leftCell, String rightCell) {
+			this.leftCell = leftCell;
+			this.rightCell = rightCell;
+		}
+
+		public void setLeftCell(String leftCell) {
+			this.leftCell = leftCell;
+		}
+
+		public void setRightCell(String rightCell) {
+			this.rightCell = rightCell;
+		}
+
+		public String getLeftCell() {
+			return this.leftCell;
+		}
+
+		public String getRightCell() {
+			return this.rightCell;
+		}
+
+		@Override
+		public String toString() {
+			return leftCell + "," + rightCell;
+		}
+
+		@Override
+		public int compareTo(TableCell other) {
+			String o1 = this.getLeftCell().toLowerCase() + this.getRightCell().toLowerCase();
+			String o2 = other.getLeftCell().toLowerCase() + other.getRightCell().toLowerCase();
+
+			return o1.compareTo(o2);
+		}
+	}
