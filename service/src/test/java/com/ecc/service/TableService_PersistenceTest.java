21e108b16d10126fda8291017c7474ce7ddfffd5
Dominic Orga <dominicorga@gmail.com>
2018-10-31 14:07:42 +0800

    Test persistence on buffer then on file

--- service/src/test/java/com/ecc/service/TableService_PersistenceTest.java
+++ service/src/test/java/com/ecc/service/TableService_PersistenceTest.java
@@ -12,6 +12,7 @@ import org.junit.Ignore;
 import org.apache.commons.io.FileUtils;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.io.File;
 import java.io.IOException;
 import java.io.FileNotFoundException;
@@ -35,9 +36,10 @@ public class TableService_PersistenceTest {
 
 		this.tableService.editCell(0, 0, true, "abcde/");
 
-		this.tableService = new TableServiceImpl(this.resourcePath.get());
-		Optional<TableCell> cell = this.tableService.getCell(0, 0);
-		assertThat(cell.get().getLeftCell()).isEqualTo("a");
+		assertOnTableBufferThenTableFilePersistence(() -> {
+			Optional<TableCell> cell = this.tableService.getCell(0, 0);
+			assertThat(cell.get().getLeftCell()).isEqualTo("a");
+		});
 	}
 
 	@Test
@@ -46,20 +48,22 @@ public class TableService_PersistenceTest {
 
 		this.tableService.editCell(0, 0, true, "ab,cde");
 
-		this.tableService = new TableServiceImpl(this.resourcePath.get());
-		Optional<TableCell> cell = this.tableService.getCell(0, 0);
-		assertThat(cell.get().getLeftCell()).isEqualTo("a");			
+		assertOnTableBufferThenTableFilePersistence(() -> {
+			Optional<TableCell> cell = this.tableService.getCell(0, 0);
+			assertThat(cell.get().getLeftCell()).isEqualTo("a");
+		});		
 	}
 
 	@Test
 	public void givenValidStringWhenEditCellThenPersistValue() 
 		throws IOException, FileNotFoundException {
 
-		this.tableService.editCell(0, 1, true, "abcde");
+		this.tableService.editCell(0, 1, true, "abcde"); 
 
-		this.tableService = new TableServiceImpl(this.resourcePath.get());
-		Optional<TableCell> cell = this.tableService.getCell(0, 1);
-		assertThat(cell.get().getLeftCell()).isEqualTo("abcde");
+		assertOnTableBufferThenTableFilePersistence(() -> {
+			Optional<TableCell> cell = this.tableService.getCell(0, 1);
+			assertThat(cell.get().getLeftCell()).isEqualTo("abcde");
+		});
 	}
 
 	@Test
@@ -68,10 +72,10 @@ public class TableService_PersistenceTest {
 
 		this.tableService.editCell(2, 1, true, "abcde");
 
-		this.tableService = new TableServiceImpl(this.resourcePath.get());
-		Optional<TableCell> cell = this.tableService.getCell(2, 1);
-
-		assertThat(cell.isPresent()).isFalse();
+		assertOnTableBufferThenTableFilePersistence(() -> {
+			Optional<TableCell> cell = this.tableService.getCell(2, 1);
+			assertThat(cell.isPresent()).isFalse();
+		});
 	}
 
 	@Test
@@ -81,14 +85,13 @@ public class TableService_PersistenceTest {
 		int rowCount = this.tableService.getRowCount();
 		this.tableService.addRow();
 
-		this.tableService = new TableServiceImpl(this.resourcePath.get());
-
-		int newRowCount = this.tableService.getRowCount();
-
-		assertThat(newRowCount).isEqualTo(rowCount + 1);
-		assertThat(this.tableService.isCellNull(rowCount, 0)).isFalse();
-		assertThat(this.tableService.isCellNull(rowCount, 1)).isTrue();
-		assertThat(this.tableService.isCellNull(rowCount, 2)).isTrue();
+		assertOnTableBufferThenTableFilePersistence(() -> {
+			int newRowCount = this.tableService.getRowCount();
+			assertThat(newRowCount).isEqualTo(rowCount + 1);
+			assertThat(this.tableService.isCellNull(rowCount, 0)).isFalse();
+			assertThat(this.tableService.isCellNull(rowCount, 1)).isTrue();
+			assertThat(this.tableService.isCellNull(rowCount, 2)).isTrue();
+		});
 	}
 
 	@Test
@@ -97,9 +100,9 @@ public class TableService_PersistenceTest {
 
 		this.tableService.addCell(2, 1, "leftString", "rightString");
 
-		this.tableService = new TableServiceImpl(this.resourcePath.get());
-
-		assertThat(this.tableService.isCellNull(2, 1)).isFalse();
+		assertOnTableBufferThenTableFilePersistence(() -> {
+			assertThat(this.tableService.isCellNull(2, 1)).isFalse();
+		});
 	}
 
 	@Test
@@ -108,11 +111,11 @@ public class TableService_PersistenceTest {
 
 		this.tableService.addCell(0, 0, "leftString", "rightString");
 
-		this.tableService = new TableServiceImpl(this.resourcePath.get());
-		Optional<TableCell> cell = this.tableService.getCell(0, 0);
- 	
-		assertThat(cell.get().getLeftCell()).isNotEqualTo("leftString");
-		assertThat(cell.get().getRightCell()).isNotEqualTo("rightString");
+		assertOnTableBufferThenTableFilePersistence(() -> {
+			Optional<TableCell> cell = this.tableService.getCell(0, 0);
+			assertThat(cell.get().getLeftCell()).isNotEqualTo("leftString");
+			assertThat(cell.get().getRightCell()).isNotEqualTo("rightString");
+		});	
 	}
 
 	@Test
@@ -121,16 +124,17 @@ public class TableService_PersistenceTest {
 
 		this.tableService.sortRow(0, true);
 
-		this.tableService = new TableServiceImpl(this.resourcePath.get());
-		Optional<TableCell> col0 = this.tableService.getCell(0, 0);
-		Optional<TableCell> col1 = this.tableService.getCell(0, 1);
-		Optional<TableCell> col2 = this.tableService.getCell(0, 2); 
-		Optional<TableCell> col3 = this.tableService.getCell(0, 3); 
+		assertOnTableBufferThenTableFilePersistence(() -> {
+			Optional<TableCell> col0 = this.tableService.getCell(0, 0);
+			Optional<TableCell> col1 = this.tableService.getCell(0, 1);
+			Optional<TableCell> col2 = this.tableService.getCell(0, 2); 
+			Optional<TableCell> col3 = this.tableService.getCell(0, 3); 
 
-		assertThat(col0.get().toString(TableService.CELL_DELIMITER + Utility.EMPTY_STRING)).isEqualTo("a,a");
-		assertThat(col1.get().toString(TableService.CELL_DELIMITER + Utility.EMPTY_STRING)).isEqualTo("bb,bb");
-		assertThat(col2.get().toString(TableService.CELL_DELIMITER + Utility.EMPTY_STRING)).isEqualTo("ccc,ccc");
-		assertThat(col3.isPresent()).isFalse();				
+			assertThat(col0.get().toString(TableService.CELL_DELIMITER + Utility.EMPTY_STRING)).isEqualTo("a,a");
+			assertThat(col1.get().toString(TableService.CELL_DELIMITER + Utility.EMPTY_STRING)).isEqualTo("bb,bb");
+			assertThat(col2.get().toString(TableService.CELL_DELIMITER + Utility.EMPTY_STRING)).isEqualTo("ccc,ccc");
+			assertThat(col3.isPresent()).isFalse();	
+		});			
 	}
 
 	@Test
@@ -138,17 +142,18 @@ public class TableService_PersistenceTest {
 		throws IOException, FileNotFoundException {
 
 		this.tableService.sortRow(0, false);
+	
+		assertOnTableBufferThenTableFilePersistence(() -> {
+			Optional<TableCell> col0 = this.tableService.getCell(0, 0);
+			Optional<TableCell> col1 = this.tableService.getCell(0, 1);
+			Optional<TableCell> col2 = this.tableService.getCell(0, 2); 
+			Optional<TableCell> col3 = this.tableService.getCell(0, 3); 
 
-		this.tableService = new TableServiceImpl(this.resourcePath.get());
-		Optional<TableCell> col0 = this.tableService.getCell(0, 0);
-		Optional<TableCell> col1 = this.tableService.getCell(0, 1);
-		Optional<TableCell> col2 = this.tableService.getCell(0, 2); 
-		Optional<TableCell> col3 = this.tableService.getCell(0, 3); 
-
-		assertThat(col0.get().toString(TableService.CELL_DELIMITER + Utility.EMPTY_STRING)).isEqualTo("ccc,ccc");
-		assertThat(col1.get().toString(TableService.CELL_DELIMITER + Utility.EMPTY_STRING)).isEqualTo("bb,bb");
-		assertThat(col2.get().toString(TableService.CELL_DELIMITER + Utility.EMPTY_STRING)).isEqualTo("a,a");
-		assertThat(col3.isPresent()).isFalse();				
+			assertThat(col0.get().toString(TableService.CELL_DELIMITER + Utility.EMPTY_STRING)).isEqualTo("ccc,ccc");
+			assertThat(col1.get().toString(TableService.CELL_DELIMITER + Utility.EMPTY_STRING)).isEqualTo("bb,bb");
+			assertThat(col2.get().toString(TableService.CELL_DELIMITER + Utility.EMPTY_STRING)).isEqualTo("a,a");
+			assertThat(col3.isPresent()).isFalse();		
+		});				
 	}
 
 	@Test
@@ -157,16 +162,17 @@ public class TableService_PersistenceTest {
 
 		this.tableService.resetTable(2, 2);
 
-		this.tableService = new TableServiceImpl(this.resourcePath.get());
-		Optional<TableCell> cell00 = this.tableService.getCell(0, 0);
-		Optional<TableCell> cell01 = this.tableService.getCell(0, 1);
-		Optional<TableCell> cell10 = this.tableService.getCell(1, 0); 
-		Optional<TableCell> cell11 = this.tableService.getCell(1, 1); 
+		assertOnTableBufferThenTableFilePersistence(() -> {
+			Optional<TableCell> cell00 = this.tableService.getCell(0, 0);
+			Optional<TableCell> cell01 = this.tableService.getCell(0, 1);
+			Optional<TableCell> cell10 = this.tableService.getCell(1, 0); 
+			Optional<TableCell> cell11 = this.tableService.getCell(1, 1); 
 
-		assertThat(cell00.isPresent()).isTrue();
-		assertThat(cell01.isPresent()).isTrue();
-		assertThat(cell10.isPresent()).isTrue();
-		assertThat(cell11.isPresent()).isTrue();
+			assertThat(cell00.isPresent()).isTrue();
+			assertThat(cell01.isPresent()).isTrue();
+			assertThat(cell10.isPresent()).isTrue();
+			assertThat(cell11.isPresent()).isTrue();	
+		});		
 	}
 
 	@Test
@@ -177,10 +183,23 @@ public class TableService_PersistenceTest {
 
 		assertThat(tableService.getRowCount()).isEqualTo(3);
 		assertThat(tableService.getColCount()).isEqualTo(4);
+
+		assertOnTableBufferThenTableFilePersistence(() -> {
+			assertThat(tableService.getRowCount()).isEqualTo(3);
+			assertThat(tableService.getColCount()).isEqualTo(4);
+		});	
 	}
 
 	@After
 	public void RestoreTable() throws IOException {
 		FileUtils.writeStringToFile(new File(this.resourcePath.get()), fileCache);		
+	}
+
+	public void assertOnTableBufferThenTableFilePersistence(Runnable runnable) 
+		throws IOException, FileNotFoundException{
+		
+		runnable.run();
+		this.tableService = new TableServiceImpl(this.resourcePath.get());
+		runnable.run();
 	}
 }
